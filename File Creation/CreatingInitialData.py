b4ec67d6647afa2a3d981a6779f9b16305f887db
mz123analysis <myron1227@gmail.com>
2025-05-19 09:24:01 -0400

    Finished Creating a Raw Dataset we will be using

--- File Creation/CreatingInitialData.py
+++ File Creation/CreatingInitialData.py
@@ -1,17 +1,44 @@
 import numpy as np
 import pandas as pd
 from datetime import date
+import random
 
 # We will create sample data for an imaginary hotel, Create Past Dates, 
 # Room Sold, Revenue, Competitor Rooms Sold, and Competitor Revenue
 #
+random.seed(10) # Ensure we get the same numbers
 
 numdays = 1000
 todaysDate = date.today()
 dates = [todaysDate - pd.Timedelta(days=x) for x in range(numdays)]
-
-for x in dates:
-    print(x)
     
-# We will have multiple hotels
-Initial_Hotel = "Imaginary Hotel"
+# We will have multiple hotels. Five imaginary hotels
+Initial_Hotel = "Imaginary Hotel "
+Hotels = [Initial_Hotel + str(x + 1) for x in range(5)]
+
+dataset = pd.DataFrame(columns=["Hotel","Total Rooms", "Date", "Rooms Solds", "Rooms Revenue","CompSet Total Rooms", "CompSet Rooms Sold", "CompSet Rooms Revenue"])
+
+for hotel in Hotels:
+    Staging = pd.DataFrame(columns=["Hotel","Total Rooms", "Date", "Rooms Solds", "Rooms Revenue", "CompSet Total Rooms", "CompSet Rooms Sold", "CompSet Rooms Revenue"])
+    
+    # Generating random numbers for these specific metrics
+    Total_Rooms = random.randint(150, 500)
+    Room_Sold = [random.randint(0,Total_Rooms) for _ in range(numdays)]
+    Room_Rev = [random.randint(5000, 100000) for _ in range(numdays)]
+    Comp_Rooms = random.randint(150, 2000)
+    Comp_Room_Sold = [random.randint(0,Comp_Rooms) for _ in range(numdays)]
+    Comp_Room_Rev = [random.randint(5000, 250000) for _ in range(numdays)]
+
+    # Applying them to the dataframe
+    Staging["Date"]= dates #Date is first so it gives 1000 rows.
+    Staging["Total Rooms"] = Total_Rooms
+    Staging["Hotel"] = hotel
+    Staging["Rooms Solds"] = Room_Sold
+    Staging["Rooms Revenue"] = Room_Rev
+    Staging["Compset Total Rooms"] = Comp_Rooms
+    Staging["Compset Rooms Sold"] = Comp_Room_Sold
+    Staging["Compset Rooms Revenue"] = Comp_Room_Rev
+
+    dataset = pd.concat([dataset, Staging], ignore_index=True) # Concatenates new rows
+
+dataset.to_csv("AzureDEPractice\File Creation\Test.csv")
